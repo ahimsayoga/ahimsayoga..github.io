021b9a52c6511c59765c67f0517e7fbf9c4c8b73
mccrodp <info@opensourceopenmind.ninja>
2017-02-15 21:14:33 +0000

    Simplified Elm program to skeleton.

--- Main.elm
+++ Main.elm
@@ -1,138 +1,21 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (href, class, style)
-import Material
-import Material.Scheme
-import Material.Button as Button
-import Material.Options exposing (css)
-import Bootstrap.Grid exposing (..)
-import Bootstrap.Wells exposing (..)
-
-
--- MODEL
-
-
-type alias Model =
-    { count : Int
-    , mdl :
-        Material.Model
-        -- Boilerplate: model store for any and all Mdl components you use.
-    }
-
-
-model : Model
-model =
-    { count = 0
-    , mdl =
-        Material.model
-        -- Boilerplate: Always use this initial Mdl model store.
-    }
-
-
-
--- ACTION, UPDATE
-
-
-type Msg
-    = Increase
-    | Reset
-    | Mdl (Material.Msg Msg)
-
-
-
--- Boilerplate: Msg clause for internal Mdl messages.
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Increase ->
-            ( { model | count = model.count + 1 }
-            , Cmd.none
-            )
-
-        Reset ->
-            ( { model | count = 0 }
-            , Cmd.none
-            )
-
-        -- Boilerplate: Mdl action handler.
-        Mdl msg_ ->
-            Material.update msg_ model
-
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 -- VIEW
 
 
-type alias Mdl =
-    Material.Model
-
-
-view : Model -> Html Msg
-view model =
-    div
-        [ style [ ( "padding", "2rem" ) ] ]
-        [ text ("Current count: " ++ toString model.count)
-          {- We construct the instances of the Button component that we need, one
-             for the increase button, one for the reset button. First, the increase
-             button. The first three arguments are:
-
-               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-               - An instance id (the `[0]`). Every component that uses the same model
-                 collection (model.mdl in this file) must have a distinct instance id.
-               - A reference to the elm-mdl model collection (`model.mdl`).
-
-             Notice that we do not have to add fields for the increase and reset buttons
-             separately to our model; and we did not have to add to our update messages
-             to handle their internal events.
-
-             Mdl components are configured with `Options`, similar to `Html.Attributes`.
-             The `Button.onClick Increase` option instructs the button to send the `Increase`
-             message when clicked. The `css ...` option adds CSS styling to the button.
-             See `Material.Options` for details on options.
-          -}
-        , containerFluid
-            [ row
-                [ column [ ExtraSmall Two, Small Two, Medium Four, Large Four ] []
-                , column [ ExtraSmall Ten, Small Ten, Medium Eight, Large Eight ]
-                    [ well WellLarge
-                        []
-                        [ Html.h1 []
-                            [ Html.text "Hello World!"
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        , Button.render Mdl
-            [ 0 ]
-            model.mdl
-            [ Button.onClick Increase
-            , css "margin" "0 24px"
-            ]
-            [ text "Increase" ]
-        , Button.render Mdl
-            [ 1 ]
-            model.mdl
-            [ Button.onClick Reset ]
-            [ text "Reset" ]
+temp =
+    div []
+        [ div [ class "brand" ]
+            [ text ("Ahimsa Yoga") ]
+        , div [ class "address-bar" ]
+            [ text ("Japan") ]
         ]
-        |> Material.Scheme.top
 
 
-
--- Load Google Mdl CSS. You'll likely want to do that not in code as we
--- do here, but rather in your master .html file. See the documentation
--- for the `Material` module for details.
-
-
-main : Program Never Model Msg
 main =
-    Html.program
-        { init = ( model, Cmd.none )
-        , view = view
-        , subscriptions = always Sub.none
-        , update = update
-        }
+    temp
